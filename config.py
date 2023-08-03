1651bfc9e9780e0fe3546fba24686719b224619f
anjali9050 <120652358+anjali9050@users.noreply.github.com>
2023-08-03 23:17:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,19 +15,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "23560088"))
+API_HASH = getenv("API_HASH", "999c01704d5c417749a98f4b8785fe88")
 
 ## Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("BOT_TOKEN", "6290477228:AAFyWeI_VU8Jx-oCqObhmUTs5en5dnGiawc")
 
 # SUDO USERS
 SUDO_USER = list(
-    map(int, getenv("SUDO_USER", "").split())
+    map(int, getenv("SUDO_USER", "1864861524").split())
 )  # Input type must be interger
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001958949068"))
 
 # Message to display when someone starts your bot
 PRIVATE_START_MESSAGE = getenv(
@@ -36,4 +36,4 @@ PRIVATE_START_MESSAGE = getenv(
 )
 
 # Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://baba:baba@cluster0.etssakc.mongodb.net/?retryWrites=true&w=majority")
